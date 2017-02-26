325efb708842f573d8815c560ec8cd4a26688df3
espumita <david_el_125@hotmail.com>
2017-02-26 20:40:01 +0000

    Maven project start

---
+++ servlets/servlet/src/main/java/controllers/ServletExample.java
@@ -0,0 +1,18 @@
+package controllers;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.annotation.WebServlet;
+import java.io.IOException;
+
+@WebServlet(name = "someName", urlPatterns = "/example")
+public class ServletExample extends javax.servlet.http.HttpServlet {
+
+    protected void doPost(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, IOException {
+
+    }
+
+    protected void doGet(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, IOException {
+        RequestDispatcher requestDispatcher = request.getRequestDispatcher("example.jsp");
+        requestDispatcher.forward(request, response);
+    }
+}
