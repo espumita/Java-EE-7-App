5b5cfb24aa5c75007628c85a2936a2360bedfda6
espumita <david_el_125@hotmail.com>
2017-02-27 17:50:41 +0000

    Extract to setup

--- src/test/java/utils/SessionProviderShould.java
+++ src/test/java/utils/SessionProviderShould.java
@@ -1,31 +1,33 @@
 package utils;
 
-import javafx.beans.binding.When;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.mockito.Matchers;
-import org.mockito.Mockito;
 import org.mockito.runners.MockitoJUnitRunner;
 
 import javax.servlet.http.HttpSession;
 
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.MatcherAssert.assertThat;
-import static org.mockito.Matchers.*;
 import static org.mockito.Matchers.any;
 import static org.mockito.Matchers.eq;
 import static org.mockito.Mockito.*;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.verify;
 
 @RunWith(MockitoJUnitRunner.class)
 public class SessionProviderShould {
 
 
+    private HttpSession httpSession;
+    private SessionProvider sessionProvider;
+
+    @Before
+    public void setUp() throws Exception {
+        httpSession = mock(HttpSession.class);
+        sessionProvider = new SessionProvider(httpSession);
+    }
+
     @Test
     public void login_an_user() {
-        HttpSession httpSession = mock(HttpSession.class);
-        SessionProvider sessionProvider = new SessionProvider(httpSession);
         User user = new User("someUserName");
 
         sessionProvider.login(user);
@@ -36,8 +38,6 @@ public class SessionProviderShould {
 
     @Test
     public void logout_an_user() throws Exception {
-        HttpSession httpSession = mock(HttpSession.class);
-        SessionProvider sessionProvider = new SessionProvider(httpSession);
 
         sessionProvider.logout();
 
@@ -47,8 +47,6 @@ public class SessionProviderShould {
 
     @Test
     public void tell_us_if_user_is_not_logged() throws Exception {
-        HttpSession httpSession = mock(HttpSession.class);
-        SessionProvider sessionProvider = new SessionProvider(httpSession);
         when(httpSession.getAttribute(any(String.class))).thenReturn(null);
 
         boolean userLogged = sessionProvider.isUserLogged();
@@ -59,8 +57,6 @@ public class SessionProviderShould {
 
     @Test
     public void tell_us_if_user_is_logged() throws Exception {
-        HttpSession httpSession = mock(HttpSession.class);
-        SessionProvider sessionProvider = new SessionProvider(httpSession);
         when(httpSession.getAttribute(any(String.class))).thenReturn("SomeSessionContent");
 
         boolean userLogged = sessionProvider.isUserLogged();
