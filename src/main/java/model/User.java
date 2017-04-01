39c65eff8a1094c73b8734c8814583d5ae6120c9
chusados <chusados@hotmail.com>
2017-04-01 11:10:50 +0100

    Refact

--- src/main/java/model/User.java
+++ src/main/java/model/User.java
@@ -1,4 +1,10 @@
 package model;
 
 public interface User {
+//entiendo que usaremos aqui el patrón sigleton no? para tener los campos que comparten las dos clases doctor y paciente
+
+
+
+
 }
+
---
+++ src/main/java/model/exceptions/Singleton.java
@@ -0,0 +1,17 @@
+package model.exceptions;
+
+
+import model.User;
+
+public class Singleton implements User {
+
+    private String name;
+    private String gender;
+    private String age;
+    private String id;
+    private static User user;
+
+    public String getInstance (){
+        return null;
+    }
+}
