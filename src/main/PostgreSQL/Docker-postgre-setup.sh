66d857c8beb220c0185c43930b9628ce053997ae
espumitadk <davidfalconnaranjo@gmail.com>
2017-05-04 18:35:51 +0100

    scripting is fun but dangerous

--- src/main/PostgreSQL/Docker-postgre-setup.sh
+++ src/main/PostgreSQL/Docker-postgre-setup.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 DEPLOY_CONFIG_PATH=~/IdeaProjects/medical-app-java-ee/src/main/PostgreSQL
-docker rmi postgres-img
-docker build -t postgres-img $DEPLOY_CONFIG_PATH
 docker stop postgres-cont
 docker rm postgres-cont
+docker rmi postgres-img
+docker build -t postgres-img $DEPLOY_CONFIG_PATH
 docker run --name postgres-cont -p 5432:5432 -d postgres-img
