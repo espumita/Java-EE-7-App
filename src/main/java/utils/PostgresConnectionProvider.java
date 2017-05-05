1a8bd38973c93ebd99f3b9181e53c3381cb8f837
espumitadk <davidfalconnaranjo@gmail.com>
2017-05-05 12:12:04 +0100

    Refactor: extract field connection

--- src/main/java/utils/PostgresConnectionProvider.java
+++ src/main/java/utils/PostgresConnectionProvider.java
@@ -1,17 +1,21 @@
 package utils;
 
-import org.omg.CORBA.TRANSACTION_MODE;
-
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class PostgresConnectionProvider {
+
+    private Connection connection = null;
+
+    public Connection provide() {
+        return connection;
+    }
+
     public boolean connect() throws ClassNotFoundException, SQLException {
 
         try {
             Class.forName("org.postgresql.Driver");
-            Connection connection = null;
             connection = DriverManager.getConnection(
                     "jdbc:postgresql://localhost:5432/GlucoseAppDB","postgres", "mysecretpassword");
             if (connection == null) return false;
