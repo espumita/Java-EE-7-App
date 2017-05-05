1394b5eb2bbe7571df6de6ddbf8a95180e2409ac
espumitadk <davidfalconnaranjo@gmail.com>
2017-05-05 10:29:29 +0100

    Timer Schedule implemented

--- ejbs/src/main/java/beans/LogBean.java
+++ ejbs/src/main/java/beans/LogBean.java
@@ -5,6 +5,7 @@ import utils.Log;
 import javax.annotation.PostConstruct;
 import javax.ejb.Lock;
 import javax.ejb.LockType;
+import javax.ejb.Schedule;
 import javax.ejb.Singleton;
 import java.io.File;
 import java.io.FileOutputStream;
@@ -23,5 +24,12 @@ public class LogBean implements LogBeanInterface{
     public void log(String name) throws Exception {
         new Log(new PrintWriter(new FileOutputStream(
                 new File("/home/david/bad-login-log.txt"), true ))).logException(name);
+    }
+
+    @Schedule(second = "59", minute = "59", hour = "23")
+    @Lock
+    public void logReport(String number) throws  Exception {
+        new Log(new PrintWriter(new FileOutputStream(
+                new File("/home/david/report-log.txt"), true ))).logException(number);
     }
 }
